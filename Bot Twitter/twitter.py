7b9dca584b523e66ce4bc4730c3785cabb560a8a
Luan Maia <luan.dias@dcomp.sor.ufscar.br>
2017-07-18 16:28:28 -0300

    Melhorias na detecção e modularização - Twitter
    Além da troca de 1 dia para 8 horas de tweets

--- Bot Twitter/twitter.py
+++ Bot Twitter/twitter.py
@@ -3,19 +3,40 @@ import tweepy, datetime, time
 #localizacao = "-23.5062,-47.4559,17km" #coordenada de Sorocaba
 localizacao = "-22.4249222,-46.939116,17km" #coordenada de Mogi Mirim para teste
 
+
+hashtagsEmergenciais = ["enchente", "terremoto", "tempestade"]
+
+indiciosTerremoto = ["tremores", "tremor"]
+indiciosTempestade = ["chuva forte", "ficando AND escuro", "aumento AND temperatura", "aumentando AND temperatura", "ficando AND quente", "escuro AND derrepente", "quente AND derrepente"]
+hashtagsNormais = indiciosTerremoto + indiciosTempestade
+
+
+
+
 def publicarTweet (api, hashtag, emergencia):
 	if emergencia == True:
-		tweet = "Possivelmente está ocorrendo " + retornarEvento(hashtag) + " em Sorocaba. Tome cuidado! " + str(datetime.datetime.now())
+		tweet = "Possivelmente está ocorrendo " + retornarEvento(hashtag) + " em Sorocaba. Tome cuidado! "
+		
+		if retornarEvento(hashtag) == "tempestade":
+			tweet = tweet + "Por causa da tempestade, pode haver enchentes. "
+		
+		tweet = tweet + str(datetime.datetime.now())
 	else:
 		tweet = "Possivelmente irá ocorrer " + retornarEvento(hashtag) + " em Sorocaba. Tome cuidado! " + str(datetime.datetime.now())
 	status = api.update_status(status=tweet)
 
 def retornarEvento (hashtag):
-	if hashtag == "furacao":
-		return "furacão"
-	elif hashtag == "tremores" or hashtag == "tremor":
+	if hashtag in indiciosTerremoto:
 		return "terremoto"
-	elif hashtag == "chuva forte":
+	elif hashtag in indiciosTempestade:
+		return "tempestade"
+	else:
+		return hashtag
+
+def retornarEventoSemAcentos (hashtag):
+	if hashtag in indiciosTerremoto:
+		return "terremoto"
+	elif hashtag in indiciosTempestade:
 		return "tempestade"
 	else:
 		return hashtag
@@ -24,24 +45,38 @@ def verificarEmergencia (api, hashtags, ultimaOcorrencia):
 	for hashtag in hashtags:
 		ultimosTweets = tweepy.Cursor(api.search, q=(hashtag), geocode=localizacao).items(1)
 		for tweet in ultimosTweets:
-			if ( ( not (hashtag in ultimaOcorrencia) ) or ( ultimaOcorrencia[hashtag] - datetime.datetime.now() ).days > 0 ) and ( tweet.created_at - datetime.datetime.now() ).days == 0:
+			if ( ( not (hashtag in ultimaOcorrencia) ) or ultimaOcorrencia[hashtag] < datetime.datetime.now()-datetime.timedelta(hours=8) ) and tweet.created_at-datetime.timedelta(hours=3) >= datetime.datetime.now()-datetime.timedelta(hours=8):
 				print (hashtag)
-				ultimaOcorrencia[hashtag] = tweet.created_at
+				ultimaOcorrencia[hashtag] = tweet.created_at-datetime.timedelta(hours=3)
+				print (ultimaOcorrencia[hashtag] + datetime.timedelta(hours=8))
 				publicarTweet (api, hashtag, True)
 
-def verificar (api, hashtags, ultimaOcorrencia):
+def verificar (api, hashtags, ultimaOcorrencia, qtdeMin):
+	qtde = {}
+	tweetsLidos = {}
+
 	for hashtag in hashtags:
-		qtde = 0
-		ultimosTweets = tweepy.Cursor(api.search, q=(hashtag), geocode=localizacao).items(5)
+		indice = retornarEventoSemAcentos(hashtag)
+		qtde[indice] = 0
+
+	for hashtag in hashtags:
+		indice = retornarEventoSemAcentos(hashtag)
+		ultimosTweets = tweepy.Cursor(api.search, q=(hashtag), geocode=localizacao).items(qtdeMin)
 
 		for tweet in ultimosTweets:
-			if ( ( not (hashtag in ultimaOcorrencia) ) or ( ultimaOcorrencia[hashtag] - datetime.datetime.now() ).days > 0 ) and ( tweet.created_at - datetime.datetime.now() ).days == 0:
-				print (hashtag)
-				qtde += 1
+			if ( ( not (hashtag in ultimaOcorrencia) ) or ultimaOcorrencia[hashtag] < datetime.datetime.now()-datetime.timedelta(hours=8) ) and tweet.created_at-datetime.timedelta(hours=3) >= datetime.datetime.now()-datetime.timedelta(hours=8):
+				if not tweet.id in tweetsLidos:
+					print (hashtag)
+					tweetsLidos[tweet.id] = 1
+					qtde[indice] += 1
 		
-		if qtde >= 5:
-			if ( not (hashtag in ultimaOcorrencia) ) or ultimaOcorrencia[hashtag] < tweet.created_at:
-					ultimaOcorrencia[hashtag] = tweet.created_at
+		for tweet in ultimosTweets:
+			break
+
+		if qtde[indice] >= qtdeMin:
+			if ( not (hashtag in ultimaOcorrencia) ) or ultimaOcorrencia[hashtag] < tweet.created_at-datetime.timedelta(hours=3):
+				ultimaOcorrencia[hashtag] = tweet.created_at-datetime.timedelta(hours=3)
+				print (ultimaOcorrencia[hashtag] + datetime.timedelta(hours=8))
 			
 			publicarTweet (api, hashtag, False)
 
@@ -60,32 +95,38 @@ def main():
 	}
 
 	api = get_api(cfg)
+	'''
+	ultimosTweets = tweepy.Cursor(api.search, q=("chuva forte 3")).items(10)
 
+	for tweet in ultimosTweets:
+		print ((tweet.created_at-datetime.timedelta(hours=3) - datetime.datetime.now()).days)
+		print (tweet.created_at-datetime.timedelta(hours=3), tweet.text, tweet.lang)
+	'''
 	# tweet = "Hello, world!"
 	# status = api.update_status(status=tweet) 
 	# Yes, tweet is called 'status' rather confusing
 
 #--------Inicialização
-	hashtagsEmergenciais = ["enchente", "furacao", "terremoto", "tempestade"]
-	hashtagsNormais = ["tremor", "tremores", "chuva forte"]
 	ultimaOcorrencia = {}
 
 #--------Looping
+
 	while True:
 		try:
+			print ("Realizando verificacoes")
 			verificarEmergencia(api, hashtagsEmergenciais, ultimaOcorrencia)
-			verificar (api, hashtagsNormais, ultimaOcorrencia)
+			verificar (api, hashtagsNormais, ultimaOcorrencia, 5)
+			print ("Verificacoes realizadas\n")
 		except Exception as e:
+			print ("\n----------------------------------------------------------------------------------------")
+			print ("Erro: " + str(e))
+			print ("Sera realizada uma nova tentativa.")
+			print ("----------------------------------------------------------------------------------------\n")
 			api = get_api(cfg)
 			continue
-		
-		time.sleep(120)
+	
+		time.sleep(180)
 
-#	ultimosTweets = tweepy.Cursor(api.search, q=("sorocaba"), geocode=localizacao).items(10)
-
-#	for tweet in ultimosTweets:
-#		print ((tweet.created_at - datetime.datetime.now()).days)
-#		print (tweet.created_at, tweet.text, tweet.lang)
 
 if __name__ == "__main__":
 	main()
